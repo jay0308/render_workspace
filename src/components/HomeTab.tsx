a0b4014ab4dbc35c67686ab55c539968f5a1dacc
Mrityunjay Kumar Karn <tm022@go-mmt.com>
2025-06-16 11:38:49 +0530

    some fixes

--- src/components/HomeTab.tsx
+++ src/components/HomeTab.tsx
@@ -35,7 +35,7 @@ const HomeTab: React.FC<HomeTabProps> = ({
   // Helper to get month name from last match
   const getLastMatchMonth = () => {
     if (!matches.length) return "";
-    const lastMatch = matches[0];
+    const lastMatch = matches[matches.length - 1];
     const dateStr = lastMatch?.matchSummary?.startDateTime;
     if (!dateStr) return "";
     const date = new Date(dateStr);
